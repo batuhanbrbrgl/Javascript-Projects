0ca70916d1f4dbbcb0dd9228383ac4be9b34d2a8
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-15 18:59:33 +0300

    js

---
+++ window.js
@@ -0,0 +1,18 @@
+// console.log(window);
+// const cevap=confirm("emin misiniz?") ;
+// if(confirm("emin misiniz?")){
+//     console.log("sil");
+
+// } 
+// else{
+//     console.log("silme");
+// }
+// const cevap=prompt("2+2=?");
+// if(cevap==4){
+//     console.log("işlem doğrudur");
+// }
+// else{
+//     console.log("işlem yanlıştır");
+
+// }
+// value = window.location.hostname;
