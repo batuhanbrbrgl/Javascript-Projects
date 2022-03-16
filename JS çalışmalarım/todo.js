228e6fa96ff0cd1b98ea2a402aa9b0a91265b832
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-17 01:04:17 +0300

    js

---
+++ JS çalışmalarım/todo.js
@@ -0,0 +1,59 @@
+// // let element;
+
+// // element=document.getElementById("todo-form");
+
+
+// // console.log(element);
+// let value;
+
+// value=document.scripts;
+// value=document.scripts.length;
+
+// value=document.links;
+// value=document.links[0];
+// value=document.links[document.links.length-1].getAttribute("class");
+// value=document.links[document.links.length-1].getAttribute("href");
+// value=document.links[document.links.length-1].className;
+
+
+// console.log(value);
+
+// let element;
+
+// element=document.getElementById("todo-form");
+// element=document.getElementById("todos-title");
+
+// element=document.getElementsByClassName("list-group-item");
+// element=document.getElementsByClassName("card-header");
+
+// element=document.getElementsByTagName("div");
+// element=document.getElementsByTagName("li");
+
+// element=document.querySelectorAll(".list-group-item");
+// element.forEach(function(el){
+
+//     console.log(el);
+// )}
+
+
+
+// console.log(element);
+
+const element = document.querySelector("#clear-todos");
+
+// console.log(element);
+// console.log(element.id);
+// console.log(element.className);
+// console.log(element.classList);
+// console.log(element.classList[1]);
+// console.log(element.textContent);
+// console.log(element.innerHTML);
+// console.log(element.style);
+// element.className="btn btn-primary";
+// element.style.color = "black";
+// element.style.backgroundColor="green";
+// element.style.marginLeft = "10px";
+// element.href = "http://www.google.com.tr";
+// element.target = "_blank";
+element.textContent = "sill";
+element.innerHTML="<span style= 'color:purple'>silll <span>"
