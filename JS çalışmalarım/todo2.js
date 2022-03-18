666107c513b3ca8e318c0335e2bbeb5823903164
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-18 17:22:07 +0300

    js

---
+++ JS çalışmalarım/todo2.js
@@ -0,0 +1,105 @@
+// const filtername=document.getElementById("filter");
+// const todoform=document.getElementById("todo-form");
+// todoform.addEventListener("submit" ,submitform);
+// function submitform(e) {
+//     console.log("batuhan");
+
+
+//     e.preventDefault();
+// }
+
+
+
+// filtername.addEventListener("focus", function(e){
+//     // console.log("filtername");
+//     // console.log(e.target);
+// })
+
+// // filtername.onfocus = function(){
+// //     console.log("buraya tıkladın");
+// // }
+
+
+// document.addEventListener("keyup", run);
+
+// function run(e) {
+//     // console.log("batuhan");
+//     console.log(e.key);
+// }
+
+// const todo=document.querySelector(".card-header");
+// const form=document.querySelector("#todo");
+
+// form.addEventListener("keyup", yaz);
+// function yaz(e) {
+// todo.textContent=e.target.value;
+// }
+
+// console.log(form);
+
+// console.log(todo);
+
+
+
+// const cardbody=document.querySelectorAll(".card-body")[1];
+// const title=document.querySelector("#tasks-title");
+// // title.addEventListener("mouseup" ,tıkla);
+// // title.addEventListener("dblclick" ,tıkla);
+// // title.addEventListener("click" ,tıkla);
+// // title.addEventListener("mousedown" ,tıkla);
+// // title.addEventListener("mouseover" ,tıkla);
+// // title.addEventListener("mouseover" ,tıkla);
+// cardbody.addEventListener("mouseenter" ,tıkla);
+// cardbody.addEventListener("mouseleave" ,tıkla);
+
+
+
+// function tıkla(e){
+    
+//     console.log(e.type);
+// }
+
+// console.log(cardbody);
+// console.log(title);
+
+// const filt=document.getElementById("filter");
+// // filter.addEventListener("focus",run);
+// // filter.addEventListener("blur",run);  select, cut, copy, paste
+// filter.addEventListener("paste",run);
+// function run(e){
+//     console.log(e.type);
+// }
+
+
+
+// document.addEventListener("DOMContentLoaded", reload);
+
+// function reload(e){
+//     console.log("Sayfa yüklendi");
+// }
+
+
+
+// console.log(filt);
+
+
+
+// const cardbody=document.querySelectorAll(".card-body")[1];
+// cardbody.addEventListener("click",run);
+
+// function run(e){
+//     if(e.target.className=== "fa fa-remove"){
+//         console.log("Silindi");
+//     }
+//     if(e.target.id==="filter"){
+//         console.log("Filtrelemeye bastınız");
+//     }
+//     if(e.target.id==="clear-todos"){
+//         console.log("Tüm veriler temizlendi");
+//     }
+
+
+    // console.log("tıklandı");
+}
+
+// console.log(cardbody);
