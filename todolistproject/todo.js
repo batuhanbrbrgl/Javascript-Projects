72d73941b3313ef61ae0ffa8994e3dc8adfaf083
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-22 02:53:28 +0300

    js

--- todolistproject/todo.js
+++ todolistproject/todo.js
@@ -1,7 +1,48 @@
-const form=document.querySelector("#todo-form");
-const todoinput=document.querySelector("#todo");
-const list=document.querySelector(".list-group");
-const firstbody=document.querySelectorAll(".card-body")[0];
-const secondbody=document.querySelectorAll(".card-body")[1];
-const filter=document.querySelector("#filter");
-const clearButton=document.querySelector("#clear-todos");
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardbody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners() { //Tüm event listenerlar
+    form.addEventListener("submit",addTodo);
+
+}
+function addTodo(e) {
+    const newTodo = todoInput.value.trim();
+
+    addTodoToUI(newTodo);
+    e.preventDefault();
+
+}
+function addTodoToUI(newTodo) { 
+    //string değerini list item olarak UI ye ekleyecek.
+
+    console.log(newTodo);
+    
+      //List item oluşturma                  
+    const listItem=document.createElement("li");
+
+    //link oluşturma
+    const link= document.createElement("a");
+    link.href="#";
+    link.className="delete-item";
+    link.innerHTML="<i class = 'fa fa-remove'></i>";
+    listItem.className="list-group-item d-flex justify-content-between";
+    
+    //Text node eklement
+
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+
+//Todo List'e List Item'ı ekleme
+    todoList.appendChild(listItem);
+    todoInput.value=""; 
+    
+    
+    
+}
