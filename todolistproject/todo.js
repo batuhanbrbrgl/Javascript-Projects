b228b010ea4537d669a8ca7b2e080ccdbf0ad463
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-21 18:38:04 +0300

    js

---
+++ todolistproject/todo.js
@@ -0,0 +1,7 @@
+const form=document.querySelector("#todo-form");
+const todoinput=document.querySelector("#todo");
+const list=document.querySelector(".list-group");
+const firstbody=document.querySelectorAll(".card-body")[0];
+const secondbody=document.querySelectorAll(".card-body")[1];
+const filter=document.querySelector("#filter");
+const clearButton=document.querySelector("#clear-todos");
