81aa86c5ce617d8130e3737e5751ab36b6798215
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-03-30 20:44:10 +0300

    GET DELETE POST PUT

--- arrow,bind,this/app.js
+++ arrow,bind,this/app.js
@@ -11,6 +11,8 @@ class Request {
         this.xhr.onload = () => {
             if (this.xhr.status === 200) {
                 console.log(this.xhr.responseText);
+            } else {
+                callback("Herhangi bir hata oluştu", null)
             }
 
 
@@ -19,10 +21,52 @@ class Request {
         this.xhr.send();
     }
 
+    delete(url, callback) {
+        this.xhr.open("DELETE", url);
+        this.xhr.setRequestHeader("Content-Type", "application/json")
+        this.xhr.onload = () => {
+            if (this.xhr.status == 200) {
+                callback(null, "Veri Silme İşlemi Başarılı")
+            } else {
+                callback("Hata oluştu", "Veri silinemedi")
+            }
+        }
+
+        this.xhr.send()
+    }
+
 
 
 
 }
 const request = new Request();
 
-request.get("https://jsonplaceholder.typicode.com/albums/")
+// request.get("https://jsonplaceholder.typicode.com/albums/")
+
+// request.post("https://jsonplaceholder.typicode.com/albums", { userId: 2, title: "Thriller" }, function(err, album) {
+//     if (err === null) {
+//         console.log(album)
+//     } else {
+//         console.log(err)
+//     }
+
+// })
+// request.put("https://jsonplaceholder.typicode.com/albums/10", { userId: 134, title: "Batuhan List" }, function(err, album) {
+//     if (err === null) {
+//         console.log(album)
+//     } else {
+//         console.log(err)
+//     }
+
+// })
+
+request.delete("https://jsonplaceholder.typicode.com/albums/10", function(err, response) {
+    if (err === null) {
+        // Başarılı
+        console.log(response);
+    } else {
+        // Hata
+        console.log(err);
+    }
+
+});
