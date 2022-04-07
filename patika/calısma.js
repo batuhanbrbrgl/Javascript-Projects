f6048d58345f1a7742a3f064a59f69ef393bd26b
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-04-07 22:01:41 +0300

    js

--- patika/calısma.js
+++ patika/calısma.js
@@ -1,25 +1,35 @@
-let isim = "Batuhan"
-let soyisim = "BERBEROĞLU"
-let yıl = "1999"
-let email = "hanbatu1684@gmail.com"
+// let isim = "Batuhan"
+// let soyisim = "BERBEROĞLU"
+// let yıl = "1999"
+// let email = "hanbatu1684@gmail.com"
 
 
-console.log(isim.toUpperCase())
-console.log(soyisim.toLowerCase())
-console.log(soyisim)
+// console.log(isim.toUpperCase())
+// console.log(soyisim.toLowerCase())
+// console.log(soyisim)
 
 
 
-let DOMAIN = email.slice(email.search("@") + 1)
-console.log(DOMAIN)
-const handro = DOMAIN.slice(0, DOMAIN.indexOf('.'))
+// let DOMAIN = email.slice(email.search("@") + 1)
+// console.log(DOMAIN)
+// const handro = DOMAIN.slice(0, DOMAIN.indexOf('.'))
 
-function btt() {
-    if (DOMAIN.slice(0, DOMAIN.indexOf('.')) === "gmail") {
-        console.log("Mail girildi")
-    } else {
-        console.log("Mail Yanlış Girildi")
-    }
+// function btt() {
+//     if (DOMAIN.slice(0, DOMAIN.indexOf('.')) === "gmail") {
+//         console.log("Mail girildi")
+//     } else {
+//         console.log("Mail Yanlış Girildi")
+//     }
 
-}
-btt();
+// }
+// btt();
+
+// email = email.replace('gmail.com', 'outlook.com')
+// console.log(email)
+// console.log(email.includes('@'))
+// console.log(
+//     email.endsWith('outlook.com'))
+// let fullname = prompt("Adınızı Giriniz...")
+// let btt = document.querySelector("#batu")
+
+// btt.innerHTML = `${btt.innerHTML} <small style=color:green>${fullname}</small>`
